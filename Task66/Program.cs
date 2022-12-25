6853aab16285cfe1ba3dafd69a4e043ee2cfbfe6
HappyShark <shark-tex47@yandex.ru>
2022-12-25 15:46:15 +0300

    first commit

---
+++ Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. 
+//Напишите программу, которая найдёт 
+//сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+Console.Write("Введите значение M, которая будет начальной точкой: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение N, которая будет конечной точкой: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int SumOfNumbers(int start, int end)
+{
+    if (start == end) return start;
+    return end + SumOfNumbers(start, end - 1);
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма всех элементов от {M} до {N} включительно = {SumOfNumbers(M, N)}");
---
+++ Task68/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+//с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите значение M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int AckerManFunction(int M, int N)
+{
+  if (M == 0) return N + 1;
+  else if (N == 0) return AckerManFunction(M - 1, 1);
+  else return AckerManFunction(M - 1, AckerManFunction(M, N - 1));
+}
+Console.WriteLine($"A(M,N) = {AckerManFunction(M,N)}");
